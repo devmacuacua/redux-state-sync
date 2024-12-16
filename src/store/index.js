917fc41f4c1bb65bf3acd8821dedd82c20e4eddf
Francisco Macuacua <macuacuafrancisco@gmail.com>
2024-12-16 14:51:29 +0200

    Implement Redux Sync

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,19 @@
 //store.js
 
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  createStateSyncMiddleware,
+  initMessageListener,
+} from "redux-state-sync";
 import countReducer from "./countSlice";
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     count: countReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(createStateSyncMiddleware({})),
 });
+
+initMessageListener(store);
+export default store;
